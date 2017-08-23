3ca7d46432084f34e6fa4109397a92942af60438
dingzeyu li <lidingzeyu@gmail.com>
2017-08-22 17:37:00 -0700

    IR conv

--- Utils/testIRs/testIR.m
+++ Utils/testIRs/testIR.m
@@ -1,6 +1,6 @@
 clear
 close all
-[h, Fs_IR] = audioread('Evertims_IR_Recording_binaural.wav');
+[h, Fs_IR] = audioread('Evertims_IR_Recording_binaural(4),100bounces.wav');
 [h, Fs_IR] = audioread('h-1300-to-6000.wav');
 %[h, Fs_IR] = audioread('h178_OfficeFoyer_1txts.wav');
 
@@ -8,7 +8,7 @@ if (size(h,2) == 1)
     h(:,2) = h;  
 end
 
-[x, fs] = audioread('/Users/dinli/Codes/vocal-transfer-github/data/SpEARdatabase/TIMIT/housewives_16.wav');
+[x, fs] = audioread('/Users/dinli/Codes/vocal-transfer-github/data/SpEARdatabase/TIMIT/intelligence_16.wav');
 
 x = resample(x, Fs_IR, fs);
 
@@ -19,6 +19,6 @@ y= y/max(max(abs(y))); % normalize max. amplitude to 1
 
 sound(y, Fs_IR)
 
-%audiowrite('h178.wav', y, Fs_IR)
+%audiowrite('h-evertims-100bounces.wav', y, Fs_IR)
 %audiowrite('h35.wav', y, Fs_IR)
 audiowrite('h-conf.wav', y, Fs_IR)
